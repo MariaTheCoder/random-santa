59671eacf2baf19fb2fad94ad9a394bd676736b3
Maria T. Andersen <mteilmann93@gmail.com>
2022-10-08 11:38:31 +0200

    Create a new component to list currently enlisted participants. If the list if empty, then display a string explaining that there are no participants currently.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import AddParticipant from "./Components/AddParticipant";
+import Participants from "./Components/Participants";
 
 export default function App() {
   const [list, setList] = useState([]);
@@ -8,6 +9,7 @@ export default function App() {
   return (
     <div className="App">
       <AddParticipant list={list} setList={setList} />
+      <Participants list={list} />
     </div>
   );
 }
---
+++ src/Components/Participants.js
@@ -0,0 +1,9 @@
+export default function Participants(props) {
+  return (
+    <ul className="Participants">
+      {props.list.length > 0
+        ? props.list.map((p, index) => <li key={index}>{p.name}</li>)
+        : "Currently no participants"}
+    </ul>
+  );
+}
