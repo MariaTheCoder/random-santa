f90685d64db15a90a3475f1d434f6b9b1be8f57a
Maria T. Andersen <mteilmann93@gmail.com>
2022-10-08 18:24:44 +0200

    Implement a useEffect which updates the value of a new state variable called test whenever the list gets updated. The test variable consists of the same amount of elements as the list variable, however, each element is a random integer.

--- src/Components/AssignSecretSanta.js
+++ src/Components/AssignSecretSanta.js
@@ -2,15 +2,33 @@ import { useEffect, useState } from "react";
 
 export default function AssignSecretSanta(props) {
   const [secretSantas, setSecretSantas] = useState([]);
+  const [test, setTest] = useState([]);
   console.log("secretSantas: ", secretSantas);
+  console.log("test: ", test);
 
   useEffect(() => {
     setSecretSantas(props.list);
-  }, [props]);
+  }, [props.list]);
+
+  useEffect(() => {
+    const newArray = [];
+
+    while (newArray.length < props.list.length) {
+      const randomInt = Math.floor(Math.random() * props.list.length);
+      newArray.push(randomInt);
+    }
+
+    setTest(newArray);
+  }, [props.list]);
 
   return (
     <div className="AssignSecretSanta">
       <button>Assign Secret Santa</button>
+      <div>
+        {props.list.length > 0
+          ? `${test[0]}`
+          : "Secret Santas can not be assigned"}
+      </div>
     </div>
   );
 }
