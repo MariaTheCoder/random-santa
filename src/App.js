f37d36df10d64fa863d45bac9aeb5f88e0043f19
Maria T. Andersen <mteilmann93@gmail.com>
2022-10-08 17:16:30 +0200

    Pass lists to the AssignSecretSanta component and use a useEffect hook to update secretSantas as the list state variable gets updated.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
     <div className="App">
       <AddParticipant list={list} setList={setList} />
       <Participants list={list} />
-      <AssignSecretSanta />
+      <AssignSecretSanta list={list} />
     </div>
   );
 }
--- src/Components/AssignSecretSanta.js
+++ src/Components/AssignSecretSanta.js
@@ -1,4 +1,13 @@
+import { useEffect, useState } from "react";
+
 export default function AssignSecretSanta(props) {
+  const [secretSantas, setSecretSantas] = useState([]);
+  console.log("secretSantas: ", secretSantas);
+
+  useEffect(() => {
+    setSecretSantas(props.list);
+  }, [props]);
+
   return (
     <div className="AssignSecretSanta">
       <button>Assign Secret Santa</button>
