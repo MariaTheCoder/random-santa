d9db637f69a3a039ce32d91be9f68dee2bd17b0e
Maria T. Andersen <mteilmann93@gmail.com>
2022-10-08 11:00:03 +0200

    Put the eventhandler of the event click into its own seperate function and give it a fitting name. Call the eventhandler upon button click.

--- src/Components/AddParticipant.js
+++ src/Components/AddParticipant.js
@@ -1,14 +1,19 @@
+import { click } from "@testing-library/user-event/dist/click";
 import { useState } from "react";
 
 export default function AddParticipant(props) {
   const [name, setName] = useState("");
 
+  function clickHandler() {
+    const listCopy = [...props.list];
+    listCopy.push(name);
+    props.setList(listCopy);
+  }
+
   return (
     <div className="AddParticipant">
       <input onChange={(event) => setName(event.target.value)}></input>
-      <button onClick={() => props.setList([...props.list, name])}>
-        Add to list
-      </button>
+      <button onClick={clickHandler}>Add to list</button>
     </div>
   );
 }
