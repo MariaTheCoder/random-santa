5799189569c090edd3024c074d82e4ac926ebbf9
Maria T. Andersen <mteilmann93@gmail.com>
2022-10-08 10:54:09 +0200

    Create a state variable to store multiple participants. Upon button click, store a name in the list and log the list on the console.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import "./App.css";
 import AddParticipant from "./Components/AddParticipant";
 
 export default function App() {
+  const [list, setList] = useState([]);
+  console.log("list: ", list);
   return (
     <div className="App">
-      <AddParticipant />
+      <AddParticipant list={list} setList={setList} />
     </div>
   );
 }
--- src/Components/AddParticipant.js
+++ src/Components/AddParticipant.js
@@ -1,12 +1,14 @@
 import { useState } from "react";
 
-export default function AddParticipant() {
+export default function AddParticipant(props) {
   const [name, setName] = useState("");
-  console.log(name);
 
   return (
     <div className="AddParticipant">
       <input onChange={(event) => setName(event.target.value)}></input>
+      <button onClick={() => props.setList([...props.list, name])}>
+        Add to list
+      </button>
     </div>
   );
 }
