45bd903448907f0e689c5f17b7747ff29dd18750
Maria T. Andersen <mteilmann93@gmail.com>
2022-10-08 11:41:56 +0200

    Use defensive programming! Use the optional chaining operator (?.) to check if list is even defined. If it is not, the function returns undefined instead of an error.

--- src/Components/Participants.js
+++ src/Components/Participants.js
@@ -2,7 +2,7 @@ export default function Participants(props) {
   return (
     <ul className="Participants">
       {props.list.length > 0
-        ? props.list.map((p, index) => <li key={index}>{p.name}</li>)
+        ? props.list?.map((p, index) => <li key={index}>{p.name}</li>)
         : "Currently no participants"}
     </ul>
   );
